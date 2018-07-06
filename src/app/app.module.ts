474c0d97e00fa196345bf86f4116fb3c3026842f
clara kaku <clara.kaku@embraer.com.br>
2018-07-05 22:29:40 -0300

    Angular project with bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { ProjectComponent } from './project/project.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    UserComponent,
+    ProjectComponent,
+    TimesheetComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
